6ed6dcb56c23b27cd50c646cf9fad40160ed9eb5
Christoph Boehner <cb@vorwaerts-werbung.de>
2011-12-16 14:43:30 +0100

    Basic content types setup (already usable) with corresponding default views.

---
+++ src/chromsystems.globalcontacts/chromsystems/globalcontacts/contact.py
@@ -0,0 +1,43 @@
+from five import grok
+from plone.directives import dexterity, form
+
+from zope import schema
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from zope.interface import invariant, Invalid
+
+from z3c.form import group, field
+
+from plone.namedfile.interfaces import IImageScaleTraversable
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+
+from plone.app.textfield import RichText
+
+from z3c.relationfield.schema import RelationList, RelationChoice
+from plone.formwidget.contenttree import ObjPathSourceBinder
+
+from chromsystems.globalcontacts import MessageFactory as _
+
+
+# Interface class; used to define content-type schema.
+
+class IContact(form.Schema, IImageScaleTraversable):
+    """
+    A contact person.
+    """
+
+    form.model("models/contact.xml")
+
+
+class Contact(dexterity.Item):
+    grok.implements(IContact)
+
+    # Add your class methods and properties here
+
+
+class View(grok.View):
+    grok.context(IContact)
+    grok.require('zope2.View')
+    grok.name('view')
