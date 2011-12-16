9cc71b34e6e8432ab3cbfd2a09fd034575072705
Christoph Boehner <cb@vorwaerts-werbung.de>
2011-12-16 17:29:02 +0100

    Add a listing for contained contacts to the contact folder to make it halfway usable.

--- src/chromsystems.globalcontacts/chromsystems/globalcontacts/globalcontacts.py
+++ src/chromsystems.globalcontacts/chromsystems/globalcontacts/globalcontacts.py
@@ -1,23 +1,16 @@
+from Acquisition import aq_inner
 from five import grok
 from plone.directives import dexterity, form
 
 from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
 
 from z3c.form import group, field
 
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from Products.CMFCore.utils import getToolByName
+from plone.app.contentlisting.interfaces import IContentListing
 
-from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
-
+from chromsystems.globalcontacts.contact import IContact
 from chromsystems.globalcontacts import MessageFactory as _
 
 
@@ -37,3 +30,15 @@ class View(grok.View):
     grok.context(IGlobalContacts)
     grok.require('zope2.View')
     grok.name('view')
+    
+    def update(self):
+        self.has_contacts = len(self.contained_contacts()) > 0
+
+    def contained_contacts(self):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        results = catalog(object_provides=IContact.__identifier__,
+                          path='/'.join(context.getPhysicalPath()),
+                          review_state='published',)
+        contacts = IContentListing(results)
+        return contacts
