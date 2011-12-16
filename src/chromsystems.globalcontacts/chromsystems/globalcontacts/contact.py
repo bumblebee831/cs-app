7c6b8ef52713bea141c2193ad4e0376a86cdf126
Christoph Boehner <cb@vorwaerts-werbung.de>
2011-12-16 17:17:06 +0100

    Basic schema/field definitions for the contact type

--- src/chromsystems.globalcontacts/chromsystems/globalcontacts/contact.py
+++ src/chromsystems.globalcontacts/chromsystems/globalcontacts/contact.py
@@ -17,6 +17,7 @@ from plone.app.textfield import RichText
 
 from z3c.relationfield.schema import RelationList, RelationChoice
 from plone.formwidget.contenttree import ObjPathSourceBinder
+from plone.formwidget.autocomplete import AutocompleteMultiFieldWidget
 
 from chromsystems.globalcontacts import MessageFactory as _
 
@@ -32,12 +33,30 @@ class IContact(form.Schema, IImageScaleTraversable):
         description=_(u"Enter a title for this contact."),
         required=True,
     )
+    salutation = schema.Choice(
+        title=_(u"label_salutation"),
+        values = [_(u"Frau"), _(u"Herr"), ],
+        default=u"Frau",
+        required=True,
+    )
+    image = NamedBlobFile(
+        title=_(u"Portrait Image"),
+        description=_(u"Upload a portrait of the contact person."),
+        required=True,
+    )
+    form.widget(countries=AutocompleteMultiFieldWidget)
+    countries = schema.Set(
+        title=_(u"Country"),
+        description=_(u"Please select the countries this contact is "
+                      u"responsible for"),
+        value_type = schema.Choice(
+            vocabulary=u"chromsystems.userdata.CountryList",
+        )
+    )
 
 
 class Contact(dexterity.Item):
     grok.implements(IContact)
-
-    # Add your class methods and properties here
 
 
 class View(grok.View):
