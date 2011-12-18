fc1c398c0dd842ee712737847f453e6cbaf9a011
Christoph Boehner <cb@vorwaerts-werbung.de>
2011-12-18 16:37:58 +0100

    Basic enquiry form to test auto generated form setup. We will need to decide wether we prefer a hard coded form

---
+++ src/chromsystems.globalcontacts/chromsystems/globalcontacts/enquiry.py
@@ -0,0 +1,130 @@
+from five import grok
+from plone.directives import form
+
+from zope import schema
+from z3c.form import button
+
+from Acquisition import aq_inner
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+from Products.statusmessages.interfaces import IStatusMessage
+from Products.CMFCore.interfaces import IContentish
+from chromsystems.globalcontacts import MessageFactory as _
+
+
+class IEnquiry(form.Schema):
+    """ Enquiry form schema composed of several interfaces as a
+        group form.
+    """
+    salutation = schema.Choice(
+        title=_(u"Salutation"),
+        values = [
+            _(u'Frau'),
+            _(u'Herr'), ],
+        default=u'Frau',
+        required=True,
+    )
+    firstname = schema.TextLine(
+        title=_(u"First name"),
+        required=True,
+    )
+    lastname = schema.TextLine(
+        title=_(u"Last name"),
+        required=True,
+    )
+    institution = schema.TextLine(
+        title=_(u"Institution"),
+        required=False,
+    )
+    street = schema.TextLine(
+        title=_(u"Street"),
+        required=True,
+    )
+    postalcode = schema.TextLine(
+        title=_(u"Postal Code"),
+        required=True,
+    )
+    city = schema.TextLine(
+        title=_(u"City"),
+        required=True,
+    )
+    country = schema.TextLine(
+        title=_(u"Country"),
+        required=True,
+    )
+    phone = schema.TextLine(
+        title=_(u"Phone"),
+        required=True,
+    )
+    fax = schema.TextLine(
+        title=_(u"Fax"),
+        required=True,
+    )
+    email = schema.TextLine(
+        title=_(u'Email'),
+        required=True,
+    )
+    message = schema.Text(
+        title=_(u"Message"),
+        required=False,
+    )
+
+
+class EnquiryForm(form.SchemaForm):
+    grok.context(IContentish)
+    grok.require('zope2.View')
+    grok.name('enquiry')
+
+    schema = IEnquiry
+    ignoreContext = True
+    css_class = 'overlayForm'
+
+    label = _(u"Contact")
+    description = _(u"Please fill out the form to send an enquiry.")
+
+    def updateActions(self):
+        super(EnquiryForm, self).updateActions()
+        self.actions["submit"].addClass("btn cta large")
+        self.actions['cancel'].addClass("btn large")
+
+    @button.buttonAndHandler(_(u'Send now'), name='submit')
+    def handleApply(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        self.status = self.send_email(data)
+
+    @button.buttonAndHandler(_(u'cancel'))
+    def handleCancel(self, action):
+        context = aq_inner(self.context)
+        context_url = context.absolute_url()
+        return self.request.response.redirect(context_url)
+
+    def send_email(self, data):
+        """ Construct and send an enquiry email. """
+        context_url = self.context.absolute_url()
+        mto = self.context.email
+        envelope_from = data['email']
+        subject = _(u'Regeneration Deutschland: Anfrage von %s') % data['name']
+        options = dict(email = data['email'],
+                       name = data['name'],
+                       message = data['message'],
+                       url = context_url,
+                        )
+        body = ViewPageTemplateFile("enquiry_email.pt")(self, **options)
+        # send email
+        mailhost = getToolByName(self.context, 'MailHost')
+        mailhost.send(body, mto=mto, mfrom=envelope_from, subject=subject, charset='utf-8')
+
+        IStatusMessage(self.request).addStatusMessage(
+            _(u"Your email has been forwarded.", "info")
+            )
+
+        return self.request.response.redirect(context_url)
+    
+    def contact_info(self):
+        context = aq_inner(self.context)
+        self.contact = self.request.get('contact', '')
+        if self.contact:
+            return self.contact
