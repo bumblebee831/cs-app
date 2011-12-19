dfefcedbff5d24c14454e69ea2fd83cea6650cd2
Christoph Boehner <cb@vorwaerts-werbung.de>
2011-12-20 00:46:35 +0100

    New group/user relationship update view adding all members tot he group `wordwide`

---
+++ src/chromsystems.userdata/chromsystems/userdata/group_cleanup.py
@@ -0,0 +1,22 @@
+from Acquisition import aq_inner
+from five import grok
+
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.interfaces import IContentish
+
+
+class CleanupWordwideGroup(grok.View):
+    grok.context(IContentish)
+    grok.require('cmf.ManagePortal')
+    grok.name('cleanup-wordwide-group')
+
+    def update(self):
+        context = aq_inner(self.context)
+        acl_users = getToolByName(context, 'acl_users')
+        portal_groups = getToolByName(context, 'portal_groups')
+        users = context.acl_users.getUsers()
+        for user in users:
+            portal_groups.addPrincipalToGroup(user.getUserName(), 'worldwide')
+
+    def render(self):
+        return 'Updated worldwide group to contain all user records'
