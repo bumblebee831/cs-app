e6040fb2c0c01c074298198519382e21f8122cbe
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-10-18 14:51:14 +0200

    Inlcude new shop specific packages

---
+++ src/chromsystems.shopcontent/chromsystems/shopcontent/tests.py
@@ -0,0 +1,62 @@
+import doctest
+import unittest
+
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import zcml
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+from Products.PloneTestCase.layer import onsetup
+
+import chromsystems.shopcontent
+
+OPTION_FLAGS = doctest.NORMALIZE_WHITESPACE | \
+               doctest.ELLIPSIS
+
+ptc.setupPloneSite(products=['chromsystems.shopcontent'])
+
+
+class TestCase(ptc.PloneTestCase):
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            zcml.load_config('configure.zcml',
+              chromsystems.shopcontent)
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Unit tests
+        #doctestunit.DocFileSuite(
+        #    'README.txt', package='chromsystems.shopcontent',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+        #doctestunit.DocTestSuite(
+        #    module='chromsystems.shopcontent.mymodule',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+
+        # Integration tests that use PloneTestCase
+        ztc.ZopeDocFileSuite(
+            'INTEGRATION.txt', 
+            package='chromsystems.shopcontent',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+        #ztc.FunctionalDocFileSuite(
+        #    'BROWSER.TXT',
+        #    package='chromsystems.shopcontent',
+        #    optionflags = OPTION_FLAGS,
+        #    test_class=TestCase),
+
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
