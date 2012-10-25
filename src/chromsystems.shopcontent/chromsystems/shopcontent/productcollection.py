e7c66df3fb91870cde7128d2a671f313c63182d8
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-10-25 18:22:20 +0200

    Include dnd functionality and schema update

--- src/chromsystems.shopcontent/chromsystems/shopcontent/productcollection.py
+++ src/chromsystems.shopcontent/chromsystems/shopcontent/productcollection.py
@@ -1,10 +1,12 @@
 from five import grok
+from zope import schema
 from plone.directives import dexterity, form
 
 from plone.app.textfield import RichText
-
+from plone.z3cform.textlines import TextLinesFieldWidget
 from z3c.relationfield.schema import RelationList, RelationChoice
 from plone.formwidget.contenttree import ObjPathSourceBinder
+from plone.formwidget.autocomplete import AutocompleteMultiFieldWidget
 
 from chromsystems.shopcontent.orderableitem import IOrderableItem
 
@@ -15,6 +17,7 @@ class IProductCollection(form.Schema):
     """
     A collection of orderable products
     """
+    #form.widget(relatedProducts=AutocompleteMultiFieldWidget)
     relatedProducts = RelationList(
         title=_(u"Related Products"),
         default=[],
@@ -23,6 +26,21 @@ class IProductCollection(form.Schema):
             source=ObjPathSourceBinder(
                 object_provides=IOrderableItem.__identifier__)
         ),
+        required=False,
+    )
+    form.widget(headlines=TextLinesFieldWidget)
+    headlines = schema.List(
+        title=_(u"Headlines"),
+        description=_(u"Headlines associated with this collection"),
+        value_type=schema.TextLine(
+            title=_(u"Headline"),
+        ),
+        required=False,
+    )
+    items = schema.TextLine(
+        title=_(u"Orderable Items"),
+        description=_(u"A list of headlines and related orderable items that "
+                      u"stores the actual display order"),
         required=False,
     )
     text = RichText(
@@ -40,3 +58,9 @@ class View(grok.View):
     grok.context(IProductCollection)
     grok.require('zope2.View')
     grok.name('view')
+
+
+class EditorView(grok.View):
+    grok.context(IProductCollection)
+    grok.require('cmf.ModifyPortalContent')
+    grok.name('editor-view')
--- src/chromsystems.sitetheme/chromsystems/sitetheme/resources/js/application.js
+++ src/chromsystems.sitetheme/chromsystems/sitetheme/resources/js/application.js
@@ -9,8 +9,33 @@
             // enhancement.
             return;
         }
-        // Add custom javascript calls here
-        // e.g.
-        // $('#my-gallery').scrollable();
+        $('div.sortable')
+            .drag("start", function (ev, dd) {
+                $(this).addClass('dragging');
+            })
+            .drag(function (ev, dd) {
+                var drop = dd.drop[0],
+                method = $.data(drop || {}, "drop+reorder");
+                if (drop && (drop != dd.current || method != dd.method)) {
+                    $(this)[method](drop);
+                    dd.current = drop;
+                    dd.method = method;
+                    dd.update();
+                }
+            })
+            .drag("end", function (ev, dd) {
+                $(this).removeClass('dragging');
+                alert('Dropped element' + $(this).attr('class'));
+            })
+            .drop("init", function (ev, dd) {
+                return !(this == dd.drag);
+            });
+        $.drop({
+            tolerance: function (event, proxy, target) {
+                var test = event.pageY > (target.top + target.height / 2);
+                $.data(target.elem, "drop+reorder", test ? "insertAfter" : "insertBefore");
+                return this.contains(target, [ event.pageX, event.pageY ]);
+            }
+        });
     });
 }(jQuery));
