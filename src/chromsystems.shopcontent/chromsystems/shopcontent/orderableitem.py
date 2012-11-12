3085a278093b7467fa4cc00f732db20e6484a96d
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-11-12 21:34:02 +0100

    Simplify and generalize the content types

--- src/chromsystems.shopcontent/chromsystems/shopcontent/orderableitem.py
+++ src/chromsystems.shopcontent/chromsystems/shopcontent/orderableitem.py
@@ -2,21 +2,7 @@ from five import grok
 from plone.directives import dexterity, form
 
 from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
-
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
-from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
 
 from chromsystems.shopcontent import MessageFactory as _
 
@@ -27,10 +13,6 @@ class IOrderableItem(form.Schema, IImageScaleTraversable):
     """
     A single orderable item
     """
-    title = schema.TextLine(
-        title=_(u"Title"),
-        required=True,
-    )
     product_id = schema.TextLine(
         title=_(u"Product ID"),
         required=True,
